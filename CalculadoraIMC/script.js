f0a77596c2667feb1bc01106a21902afe91a6638
Charlon Fernandes <charlon135@gmail.com>
2022-06-29 20:48:05 -0300

    Upgrades da calculadora

--- CalculadoraIMC/script.js
+++ CalculadoraIMC/script.js
@@ -3,30 +3,50 @@
     const calcular = document.getElementById('btn')
     
     function calculaImc() {
-        const nome = document.getElementById('inp-nom').value
-        const alt = document.getElementById('inp-alt').value
-        const pes = document.getElementById('inp-pes').value
-        const resultado = document.getElementById('res')
+        var nome = document.getElementById('inp-nom').value
+        var alt = document.getElementById('inp-alt').value
+        var pes = document.getElementById('inp-pes').value
+        var opc = document.getElementsByName('sex')
+        var resultado = document.getElementById('res')
         
         var classe
-
+        
+        //vericador de dados
         if (nome !== "" && alt !== "" && pes !== "") {
             const imc = (pes / (alt * 2)).toFixed(2)
 
-            if (imc >= 40) {
-                classe = "Obesidade Morbida"
-            } else if (imc >= 30){
-                classe = "Obesidade"
-            } else if (imc >= 25) {
-                classe = "Sobrepeso"
-            } else if (imc >= 18.5){
-                classe = "Peso Normal"
-            } else if (imc < 18.5){
-                classe = "abaixo do pesinho"
+            //verificar se é homem
+            if (opc[0].checked) {
+                if (imc > 43) {
+                    classe = "Obesidade Morbida"
+                } else if (imc >= 30){
+                    classe = "Obesidade"
+                } else if (imc >= 25) {
+                    classe = "Sobrepeso"
+                } else if (imc >= 20){
+                    classe = "Peso Normal"
+                } else if (imc < 20){
+                    classe = "abaixo do pesinho"
+                }
+
+            } else if (opc[1].checked) {
+                if (imc > 39) {
+                    classe = "Obesidade Morbida"
+                } else if (imc >= 29){
+                    classe = "Obesidade"
+                } else if (imc >= 24) {
+                    classe = "Sobrepeso"
+                } else if (imc >= 19){
+                    classe = "Peso Normal"
+                } else if (imc < 19){
+                    classe = "abaixo do pesinho"
+                }
+
             }
 
             resultado.innerText = `${nome} seu imc é ${imc}. Logo, sua classificação é ${classe}`
 
+            
         } else {
             resultado.innerText= "Resultado: erro"
         }
