2c6cbb9455fe9f02164440bb11ebbdf0c1dfac06
Charlon Fernandes <charlon135@gmail.com>
2022-06-29 08:46:36 -0300

    Conclusão das condições pt1

--- CalculadoraIMC/script.js
+++ CalculadoraIMC/script.js
@@ -7,24 +7,25 @@
         const alt = document.getElementById('inp-alt').value
         const pes = document.getElementById('inp-pes').value
         const resultado = document.getElementById('res')
+        
         var classe
 
         if (nome !== "" && alt !== "" && pes !== "") {
             const imc = (pes / (alt * 2)).toFixed(2)
 
-            if(imc>8000){
-
-            } else if (imc > 35) {
-                classe = "acima do peso"
-            } else if (imc < 30 && imc >= 25){
-                classe = "levemente acima do peso"
-            } else if (imc < 25 && imc >= 18.5) {
-                classe = "no peso ideal"
+            if (imc >= 40) {
+                classe = "Obesidade Morbida"
+            } else if (imc >= 30){
+                classe = "Obesidade"
+            } else if (imc >= 25) {
+                classe = "Sobrepeso"
+            } else if (imc >= 18.5){
+                classe = "Peso Normal"
             } else if (imc < 18.5){
                 classe = "abaixo do pesinho"
             }
 
-            resultado.innerText = `${nome} seu imc é ${imc}. Logo, tu se encontra ${classe}`
+            resultado.innerText = `${nome} seu imc é ${imc}. Logo, sua classificação é ${classe}`
 
         } else {
             resultado.innerText= "Resultado: erro"
