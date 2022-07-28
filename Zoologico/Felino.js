456d3212e5192fb3a998790a01ebd7b8e923c233
Charlon Fernandes <charlon135@gmail.com>
2022-07-28 08:37:49 -0300

    Criação da classe Felino

---
+++ Zoologico/Felino.js
@@ -0,0 +1,14 @@
+class Felino extends Animal {
+   
+    constructor (nome){
+        super(name);
+    }
+
+    hunt(){ 
+        console.log(`${this.getName()} is hunting`)
+    }
+
+    ronron(){
+        console.log(`${this.getName()}: rrrrrrrrr`)
+    }
+}
